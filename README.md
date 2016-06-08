e28cb95ccf45c82bdbb55b9392707401013ffe12
Bublik <bublik@dez-mac-air.local>
2016-06-08 13:55:55 +0300

    Save templates in postmark on `production` command

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # gandalf.email
 
 Email templates for [gandalf.web](https://github.com/Nebo15/gandalf.web)
+
+#### Build templates
+
+`gulp build`
+
+#### Build and save in Postmark
+
+`gulp production`
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,31 @@
-var gulp = require('gulp'),
-    emailBuilder = require('gulp-email-builder');
+'use strict';
+
+var fs = require("fs"),
+    gulp = require('gulp'),
+    gulpUtil = require('gulp-util'),
+    emailBuilder = require('gulp-email-builder'),
+    postmark = require('postmark');
+
+var postmarkConfig = require('./postmark.json'),
+    postmarkClient = new postmark.Client(postmarkConfig.token);
+
+function postmarkEditTemplate (template) {
+    return new Promise((resolve, reject) => {
+        postmarkClient.editTemplate(template.id, {
+            "Name": template.name,
+            "Subject": template.subject,
+            "TextBody": "",
+            "HtmlBody": String(fs.readFileSync(template.src))
+        }, (error, success) => {
+            if (error) {
+                reject(error);
+            } else {
+                gulpUtil.log('Success:', template.name);
+                resolve(success);
+            }
+        });
+    });
+}
 
 gulp.task('build', function () {
     return gulp.src('./src/*.html')
@@ -7,6 +33,12 @@ gulp.task('build', function () {
         .pipe(gulp.dest('./build/'))
 });
 
-gulp.task('watch', ['build'], function () {
+gulp.task('production', ['build'], callback => {
+    Promise.all(postmarkConfig.templates.map(template => {
+        return postmarkEditTemplate(template);
+    })).then(() => callback());
+});
+
+gulp.task('watch', ['build'], () => {
     gulp.watch('./src/**', ['build']);
 });
